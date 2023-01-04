247ccf2f9fa1b62a3388d0fae66585e738602119
ge86yaw <kaanguney.keklikci@tum.de>
2023-01-05 00:19:26 +0100

    add execution conditions to emergency stop pattern

--- contracts/EmergencyStop.sol
+++ contracts/EmergencyStop.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./AccessRestriction.sol";
-
 contract EmergencyStop {
 
   // declare state variables
@@ -11,18 +9,33 @@ contract EmergencyStop {
 
   constructor() {
     owner = msg.sender;
+    execute = true;
+  }
+
+  // implement std. access restriction
+  modifier onlyOwner(address account) {
+    require(msg.sender == account, "Unauthorized access!");
+    _;
+  }
+
+  // implement resume check
+  modifier onExecute(bool _execute) {
+    require(_execute, "Contract is in execution!");
+    _;
+  }
+
+  // implement pause check
+  modifier onPause(bool _execute) {
+    require(!_execute, "Contract is on pause!");
+    _;
+  }
+
+  function pause() public onlyOwner(owner) onExecute(execute) {
     execute = false;
   }
 
-  modifier onPause() {
-    require(execute, "Contract is in execution!");
-    _;
+  function resume() public onlyOwner(owner) onPause(execute) {
+    execute = true;
   }
 
-  modifier canExecute() {
-    require(!execute, "Contract is on pause!");
-    _;
-  }
-
-  // TODO: implement emergency conditions
 }
