a866d489a4ab088f798be46d8ce95ff49c447ae2
ge86yaw <kaanguney.keklikci@tum.de>
2023-01-06 20:16:06 +0100

    implement contract-based deposit & withdrawal workflow

--- contracts/EmergencyStop.sol
+++ contracts/EmergencyStop.sol
@@ -6,12 +6,14 @@ contract EmergencyStop {
   // declare state variables
   address owner;
   bool execute;
+  bool withdrawal;
   string instance;
   mapping(address => uint) balances;
 
   constructor() {
     owner = msg.sender;
     execute = true;
+    withdrawal = false;
     instance = "Emergency Stop Pattern";
   }
 
@@ -20,27 +22,58 @@ contract EmergencyStop {
     return instance;
   }
 
+  // implement getter for contract balance
+  function getContractBalance() public view returns(uint) {
+    return balances[address(this)];
+  }
+
+  // implement getter for contract address
+  function getContractAddress() public view returns(address) {
+    return address(this);
+  }
+
+  // implement getter for contract address
+  function getOwnerAddress() public view returns(address) {
+    return owner;
+  }
+
+  // implement getter for pause / resume operations
+  function getExecutionState() public view returns(bool) {
+    return execute;
+  }
+
+  // implement getter for withdrawal function test
+  function getWithdrawalState() public view returns(bool) {
+    return withdrawal;
+  }
+
+  // implement getter for owner balance
+  function getOwnerBalance() public view returns(uint) {
+    return balances[msg.sender];
+  }
+
+
   // implement std. access restriction
   modifier onlyOwner(address account) {
     require(msg.sender == account, "Unauthorized access!");
     _;
-  }
+  }  
 
   // implement resume check
   modifier onExecute(bool _execute) {
-    require(_execute, "Contract is in execution!");
+    require(_execute == true, "Contract is in execution!");
     _;
   }
 
   // implement pause check
   modifier onPause(bool _execute) {
-    require(!_execute, "Contract is on pause!");
+    require(_execute == false, "Contract is on pause!");
     _;
   }
 
   // implement balance check
   modifier balanceCheck(uint amount) {
-    require(balances[msg.sender] > amount, "Insufficient funds for withdrawal!");
+    require(balances[address(this)] > amount, "Insufficient funds for withdrawal!");
     _;
   }
 
@@ -52,17 +85,13 @@ contract EmergencyStop {
     execute = true;
   }
 
-  // deposit ether
-  function deposit() public onExecute(execute) payable {
-    balances[msg.sender] += msg.value;
+  // deposit ether to the contract
+  function deposit() external onExecute(execute) payable {
+    balances[address(this)] += msg.value;
   }
 
-  // withdraw ether
-  // TODO: add re-entrancy guard
-  function withdraw() public onPause(execute) balanceCheck(msg.value) payable {
-      (bool success, ) = msg.sender.call{value: msg.value}("");
-      require(success, "Withdrawal failed!");
-      balances[msg.sender] = 0;
+  // withdraw all funds from the contract
+  function withdraw() public onPause(execute) balanceCheck(0) payable {
+    balances[address(this)] = 0;
   }
-
 }
---
+++ test/2_emergency_stop_test.js
@@ -0,0 +1,127 @@
+// define pattern
+let pattern = "Emergency Stop Pattern";
+let artifact = pattern.replace(' Pattern', '').replace(' ', '');
+
+// load compiled contract
+const EmergencyStop = artifacts.require(artifact);
+
+// load truffle assertions
+const truffleAssert = require("truffle-assertions");
+
+// test contract
+contract(EmergencyStop, (accounts) => {
+
+    // test pattern name
+    it("should return pattern name", async () => {
+        const deployed = await EmergencyStop.deployed();
+        const instance = await deployed.getInstance();
+        assert.equal(instance, pattern);
+    });
+
+    // test owner only function modifier
+    it("should return execution state", async () => {
+        const deployed = await EmergencyStop.deployed();
+        const state = await deployed.getExecutionState();
+        assert.equal(state, true);
+    });
+
+    // test contract on pause state
+    it("should return pause state", async () => {
+        const deployed = await EmergencyStop.deployed();
+        await deployed.pause();
+        const state = await deployed.getExecutionState();
+        assert.equal(state, false);
+    });
+
+    // test contract on resume state
+    it("should return resume state", async () => {
+        const deployed = await EmergencyStop.deployed();
+        await deployed.resume();
+        const state = await deployed.getExecutionState();
+        assert.equal(state, true);
+    });
+
+    // test deposit
+    it("should return deposit state", async () => {
+        const deployed = await EmergencyStop.deployed();
+        await deployed.deposit({"from": accounts[1], value: 10});
+        const state = await deployed.getWithdrawalState();
+        assert.equal(state, false);
+    });
+
+    // pause contract
+    it("should return pause state", async () => {
+        const deployed = await EmergencyStop.deployed();
+        await deployed.pause();
+        const state = await deployed.getExecutionState();
+        assert.equal(state, false);
+    });
+
+    // should check contract balance after deposit
+    it("should return contract balance", async () => {
+        const deployed = await EmergencyStop.deployed();
+        const balance = await deployed.getContractBalance();
+        assert.equal(balance > 0, true);
+    });
+
+    // test contract on resume state
+    it("should return resume state", async () => {
+        const deployed = await EmergencyStop.deployed();
+        await deployed.resume();
+        const state = await deployed.getExecutionState();
+        assert.equal(state, true);
+    });
+
+    // test contract address
+    it("should log contract address", async () => {
+        const deployed = await EmergencyStop.deployed();
+        const address = await deployed.getContractAddress();
+        try {
+            assert.ok(address !== null);
+        } 
+        catch (error) {
+            console.log(error);
+        }
+        console.log(`Contract address: ${address}`);
+    });
+
+    // test owner address
+    it("should log owner address", async () => {
+        const deployed = await EmergencyStop.deployed();
+        const address = await deployed.getOwnerAddress();
+        try {
+            assert.ok(address !== null);
+        } 
+        catch (error) {
+            console.log(error);
+        }
+        console.log(`Owner address: ${address}`);
+    });
+    
+    // test deposit
+    it("should log valid contract balance (deposit)", async () => {
+        const deployed = await EmergencyStop.deployed();
+        const balance = await deployed.getContractBalance();
+        await deployed.deposit({"from": accounts[1], value: 10});
+        const diff = await deployed.getContractBalance() - balance;
+        assert.ok(diff > 0);
+        console.log(`Contract balance increased by (post deposit): ${diff}`);
+    });
+
+    // pause contract to test emergency withdrawal
+    it("should return pause state", async () => {
+        const deployed = await EmergencyStop.deployed();
+        await deployed.pause();
+        const state = await deployed.getExecutionState();
+        assert.equal(state, false);
+    });
+
+    // test withdrawal
+    it("should log valid contract balance", async () => {
+        const deployed = await EmergencyStop.deployed();
+        const address = await deployed.getContractAddress();
+        await deployed.withdraw();
+        const balance = await deployed.getContractBalance();
+        console.log(`Contract balance (post withdrawal): ${balance}`);
+    });
+});
